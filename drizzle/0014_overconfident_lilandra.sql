b5a5db8612be9f0f5eef2a83f003859479012f17
IndigoFenix <indigofenix00@gmail.com>
2026-02-11 14:55:24 +0200

    fixed migration issue

--- drizzle/0014_overconfident_lilandra.sql
+++ drizzle/0014_overconfident_lilandra.sql
@@ -1,8 +1,3 @@
-CREATE TABLE "system_settings" (
-	"key" text PRIMARY KEY NOT NULL,
-	"value" text NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL
-);
 --> statement-breakpoint
 ALTER TABLE "students" ADD COLUMN "aac_elevenlabs_api_key" text;--> statement-breakpoint
 ALTER TABLE "students" ADD COLUMN "aac_elevenlabs_ai_voice_id" text;--> statement-breakpoint
